60e93f93c4c329337506a368cddb712ebf7ae468
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-09 11:37:26 +0000

    Filters utilities

---
+++ src/core/filters.js
@@ -0,0 +1,39 @@
+let Filters = {};
+
+// Makes a db query from a filters array and a queryString object
+Filters.makeQuery = (filters, query) => {
+	let q = {};
+
+	filters.forEach(filter => {
+		let value = query[filter.name];
+		if (value) {
+			if (filter.type === 'toggle') {
+				if (value === '1') q[filter.name] = true;
+				else if (value === '0') q[filter.name] = false;
+			} else {
+				let options = filter.options;
+				// If filter has parents use the correct options based on parent value
+				if (filter.parent) {
+					let parent = query[filter.parent];
+					options = options[parent] || [];
+				}
+
+				if (typeof value === 'string') {
+					if (options.indexOf(value) !== -1) q[filter.name] = value;
+				} else if (Array.isArray(value)) {
+					let a = value.filter(val => options.indexOf(val) !== -1);
+					if (a.length === 1) {
+						q[filter.name] = a[0];
+					} else if (a.length > 1) {
+						if (filter.selection === 'inclusive') q[filter.name] = {$in: a};
+						else q[filter.name] = {$all: a};
+					}
+				}
+			}
+		}
+	});
+
+	return q;
+};
+
+module.exports = Filters;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-const Server = require('./core/server');
-const db = require('./core/db');
-
-let NOX = {};
-
-NOX.server = config => Server(config);
-NOX.db = db;
-
-module.exports = NOX;
 module.exports.Server = require('./core/server');
 module.exports.DB = require('./core/db');
+module.exports.Filters = require('./core/filters');
