2dd79bfe45b153542f7531c77ca1a229b2258ffc
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-19 12:02:23 +0000

    Use Models generator for Settings

--- src/models/settings.js
+++ src/models/settings.js
@@ -1,23 +1,12 @@
-const LRU = require('lru-cache');
-const db = require('../core/db');
+const Models = require('../core/models');
 
-var Settings = {};
-
-// var cache = new LRU({
-// 	max: 1,
-// 	maxAge: 1000 * 60 * 60, // 60 min
-// });
-
-// Get one brand by url slug
-Settings.get = async () => {
-	// let settings = cache.get('settings');
-	// if (!settings) {
-	let settings = await db.collection('settings').findOne({_id: 'settings'}, {projection: {_id: 0}});
-	if (!settings) throw {status: 500, message: 'Missing site settings!'};
-	// cache.set('settings', settings);
-	// }
-	return settings;
-};
+let Settings = Models.create('settings', {
+	get: {
+		method: 'get',
+		filter: {_id: 'settings'},
+		cache: {count: 1, time: 60},
+	},
+});
 
 Settings.getForClient = settings => {
 	const {filters, colors, social, homePage} = settings;
