a3050dc9a57ecc2911fabb713246090dfcfc6740
Alex Petrisor <alexgpetrisor@gmail.com>
2021-03-03 13:52:15 +0000

    No more settings

--- src/core/server.js
+++ src/core/server.js
@@ -1,19 +1,8 @@
 const Db = require('./db');
 const webserver = require('../lib/webserver2');
-const Settings = require('../models/settings');
 
 function Server(config) {
 	let app = webserver.create(config);
-	// Get site settings and append to req
-	app.use((req, res, next) => {
-		Settings.get()
-			.then(settings => {
-				req.settings = settings;
-				req.clientSettings = Settings.getForClient(settings);
-				next();
-			})
-			.catch(next);
-	});
 
 	this.start = () => {
 		Db.connect(config.mongoUrl)
