c1b573f4ff1035f1ef29d78026da5ee701e5b39e
Alex Petrisor <alexgpetrisor@gmail.com>
2021-03-03 13:52:05 +0000

    Filters can make query w/o validation

--- src/core/filters.js
+++ src/core/filters.js
@@ -1,8 +1,10 @@
 let Filters = {};
 
 // Makes a db query from a filters array and a queryString object
-Filters.makeQuery = (filters, query) => {
+Filters.makeQuery = (filters, query, validate = true) => {
 	let q = {};
+
+	if (!Array.isArray(filters)) return q;
 
 	filters.forEach(filter => {
 		let value = query[filter.name];
@@ -18,14 +20,14 @@ Filters.makeQuery = (filters, query) => {
 				}
 
 				if (typeof value === 'string') {
-					if (options.indexOf(value) !== -1) q[filter.name] = value;
+					if (!validate || options.indexOf(value) !== -1) q[filter.name] = value;
 				} else if (Array.isArray(value)) {
-					let a = value.filter(val => options.indexOf(val) !== -1);
-					if (a.length === 1) {
-						q[filter.name] = a[0];
-					} else if (a.length > 1) {
-						if (filter.selection === 'inclusive') q[filter.name] = {$in: a};
-						else q[filter.name] = {$all: a};
+					if (validate) value = value.filter(val => options.indexOf(val) !== -1);
+					if (value.length === 1) {
+						q[filter.name] = value[0];
+					} else if (value.length > 1) {
+						if (filter.selection === 'inclusive') q[filter.name] = {$in: value};
+						else q[filter.name] = {$all: value};
 					}
 				}
 			}
